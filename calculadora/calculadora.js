2e727d90202f00a0fb72ef94d297c139ee72b20e
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-04-19 23:51:37 -0500

    feat: Added operation with negative numbers

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -5,21 +5,33 @@ const keyOperators = document.querySelectorAll('.key--operator');
 const keyDecimal = document.querySelector('.key--decimal');
 const keyEqual = document.querySelector('.key--equal');
 
-let firstNumber;
-let operatorClicked;
-let lastKeyType;
+const DISPLAY_INIT_STATE = '';
+const NUMBER_KEY = 'number';
+const OPERATOR_KEY = 'operator';
+const EQUAL_KEY = 'equal';
+const DECIMAL_KEY = 'decimal';
+const MINUS_KEY = 'minus';
 
-setInitialState();
+changeDisplayContent(DISPLAY_INIT_STATE);
+let firstNumber = '';
+let operatorClicked = '';
+let lastKeyType = '';
 
-function setInitialState() {
-	display.textContent = 0;
-	firstNumber = '';
-	operatorClicked = '';
-	lastKeyType = '';
+function changeDisplayContent(newContent) {
+	display.textContent = newContent;
+}
+
+function addContentToDisplay(content) {
+	display.textContent += content;
+}
+
+function getDisplayContent() {
+	return display.textContent;
 }
 
 function calculate(operator, firstValue, secondValue) {
 	let res = 0;
+
 	switch (operator) {
 		case 'add':
 			res = firstValue + secondValue;
@@ -34,69 +46,106 @@ function calculate(operator, firstValue, secondValue) {
 			res = firstValue / secondValue;
 			break;
 	}
+
+	if (!isFinite(res)) {
+		return 'Indeterminado';
+	}
+
 	return res;
 }
 
 function displayTotal() {
-	display.textContent = calculate(
+	const total = calculate(
 		operatorClicked,
 		Number(firstNumber),
-		Number(display.textContent)
+		Number(getDisplayContent())
 	);
+
+	changeDisplayContent(total);
 }
 
 keyNumbers.forEach((key) => {
 	key.addEventListener('click', (event) => {
 		if (
-			display.textContent === '0' ||
-			lastKeyType === 'operator' ||
-			lastKeyType === 'equal'
+			lastKeyType !== MINUS_KEY &&
+			(getDisplayContent() === DISPLAY_INIT_STATE ||
+				lastKeyType === OPERATOR_KEY ||
+				lastKeyType === EQUAL_KEY)
 		) {
-			display.textContent = event.target.textContent;
+			changeDisplayContent(event.target.textContent);
 		} else {
-			display.textContent += event.target.textContent;
+			addContentToDisplay(event.target.textContent);
 		}
-		if (lastKeyType === 'equal') {
+
+		if (lastKeyType === EQUAL_KEY) {
 			firstNumber = '';
 		}
-		lastKeyType = 'number';
+
+		lastKeyType = NUMBER_KEY;
 	});
 });
 
 keyDecimal.addEventListener('click', () => {
-	if (lastKeyType === 'operator' || lastKeyType === 'equal') {
-		display.textContent = '0.';
+	if (lastKeyType === OPERATOR_KEY || lastKeyType === EQUAL_KEY) {
+		changeDisplayContent('0.');
 		firstNumber = '';
-	} else if (!display.textContent.includes('.')) {
-		display.textContent += '.';
+	} else if (lastKeyType === MINUS_KEY) {
+		changeDisplayContent('-0.');
+	} else if (!getDisplayContent().includes('.')) {
+		addContentToDisplay('.');
 	}
-	lastKeyType = 'decimal';
+
+	lastKeyType = DECIMAL_KEY;
 });
 
 keyOperators.forEach((key) => {
 	key.addEventListener('click', (event) => {
 		if (
+			event.target.dataset.action === 'subtract' &&
+			(lastKeyType === '' || lastKeyType === OPERATOR_KEY)
+		) {
+			changeDisplayContent('-');
+			lastKeyType = MINUS_KEY;
+			return;
+		}
+
+		if (
 			firstNumber &&
 			operatorClicked &&
-			lastKeyType !== 'operator' &&
-			lastKeyType !== 'equal'
+			lastKeyType !== OPERATOR_KEY &&
+			lastKeyType !== EQUAL_KEY &&
+			lastKeyType !== MINUS_KEY
 		) {
 			displayTotal();
 		}
-		firstNumber = display.textContent;
-		operatorClicked = event.target.dataset.action;
-		lastKeyType = 'operator';
+
+		// Avoid operations if the calculator doesn't have a valid operation on input
+		if (
+			getDisplayContent() !== DISPLAY_INIT_STATE &&
+			getDisplayContent() !== '-'
+		) {
+			firstNumber = getDisplayContent();
+			operatorClicked = event.target.dataset.action;
+			lastKeyType = OPERATOR_KEY;
+		}
 	});
 });
 
 keyClear.addEventListener('click', () => {
-	setInitialState();
-	lastKeyType = 'clear';
+	changeDisplayContent(DISPLAY_INIT_STATE);
+	firstNumber = '';
+	operatorClicked = '';
+	lastKeyType = '';
 });
 
 keyEqual.addEventListener('click', () => {
-	if (firstNumber && lastKeyType !== 'equal' && lastKeyType !== 'operator') {
+	if (
+		firstNumber &&
+		lastKeyType !== EQUAL_KEY &&
+		lastKeyType !== OPERATOR_KEY
+	) {
 		displayTotal();
 	}
-	lastKeyType = 'equal';
+
+	lastKeyType = EQUAL_KEY;
 });
