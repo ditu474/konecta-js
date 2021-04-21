caa2fb2e6c1744151e28493c5634f963faef752f
Daniel Ramirez <61102046+ditu474@users.noreply.github.com>
2021-04-21 12:12:41 -0500

    chore: Added schematics for input validation

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import {
+	isAlpha,
+	isEmail,
+	isIn,
+	isLength,
+	isMinorTime,
+	isMobilePhone,
+	isNumeric,
+	isTime,
+} from './validators.js';
+
 const form = document.getElementById('main-form');
 const idNumberInput = document.getElementById('id');
 const nameInput = document.getElementById('name');
@@ -21,10 +32,16 @@ DUMMY_LOCATIONS.forEach((location) => {
 	dataListLocations.appendChild(option);
 });
 
+const validatorWrapper = (validator, ...params) => {
+	return (val) => validator(val, ...params);
+};
+
 const formObject = {
 	valid: function () {
-		const invalidInputs = this.inputs.filter((input) => !input.valid);
-		return invalidInputs.length === 0;
+		for (const input of this.inputs) {
+			if (!input.valid) return false;
+		}
+		return true;
 	},
 	element: form,
 	inputs: [
@@ -32,51 +49,104 @@ const formObject = {
 			valid: false,
 			touched: false,
 			element: idNumberInput,
+			validators: [
+				validatorWrapper(isNumeric),
+				validatorWrapper(isLength, { min: 8, max: 11 }),
+			],
 		},
 		{
 			valid: false,
 			touched: false,
 			element: nameInput,
+			validators: [
+				validatorWrapper(isAlpha),
+				validatorWrapper(isLength, { min: 5, max: 30 }),
+			],
 		},
 		{
 			valid: false,
 			touched: false,
 			element: celInput,
+			validators: [validatorWrapper(isMobilePhone)],
 		},
 		{
 			valid: false,
 			touched: false,
 			element: emailInput,
+			validators: [validatorWrapper(isEmail)],
 		},
 		{
 			valid: false,
 			touched: false,
 			element: areaInput,
+			validators: [
+				validatorWrapper(isAlpha),
+				validatorWrapper(isLength, { min: 3 }),
+			],
 		},
 		{
 			valid: false,
 			touched: false,
 			element: cargoInput,
+			validators: [
+				validatorWrapper(isAlpha),
+				validatorWrapper(isLength, { min: 3 }),
+			],
 		},
 		{
 			valid: false,
 			touched: false,
 			element: locationInput,
+			validators: [validatorWrapper(isIn, DUMMY_LOCATIONS)],
 		},
 		{
 			valid: false,
 			touched: false,
 			element: timeSinceInput,
+			validators: [
+				validatorWrapper(isTime),
+				validatorWrapper(isMinorTime, timeUntilInput.value),
+			],
 		},
 		{
 			valid: false,
 			touched: false,
 			element: timeUntilInput,
+			validators: [validatorWrapper(isTime)],
 		},
 	],
 };
 
+formObject.inputs.forEach((input) => {
+	input.element.addEventListener('change', (evt) => {
+		const parent = input.element.parentNode;
+		let sibling = input.element.nextSibling;
+		while (sibling) {
+			const aux = sibling.nextSibling;
+			parent.removeChild(sibling);
+			sibling = aux;
+		}
+		const errors = input.validators.map((validator) =>
+			validator(evt.target.textContent)
+		);
+		if (errors.length > 0) {
+			input.invalid = false;
+			input.element.className = 'input--invalid';
+			errors.forEach((err) => {
+				const span = document.createElement('span');
+				const content = document.createTextNode(err);
+				span.appendChild(content);
+				span.className = 'input__message--invalid';
+				parent.insertBefore(span, input.element.nextSibling);
+			});
+		} else {
+			input.valid = true;
+			input.element.className = 'input--valid';
+		}
+	});
+});
+
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
-	console.log('formSubmited');
+	console.log(formObject.valid());
 });
--- validators.js
+++ validators.js
@@ -1,15 +1,34 @@
-export function isIdentityCard(id) {}
+export function isNumeric(str) {
+	return 'Debe contener solo números';
+}
 
-export function isTime(time) {}
+export function isTime(str) {
+	return 'No es un tiempo con formato HH:MM';
+}
 
-export function isEmail(email) {}
+export function isEmail(str) {
+	return 'No es un email valido';
+}
 
-export function isNotEmpty(str) {}
+export function isIn(str, values) {
+	return 'El valor no está entre las opciones posibles';
+}
 
-export function isIn(str, values) {}
+export function isLength(str, options = { min: 0, max: undefined }) {
+	if (options.max) {
+		return `Debe tener minimo ${options.min} caracteres y maximo ${options.max}`;
+	}
+	return `Debe tener minimo ${options.min} caracteres`;
+}
 
-export function isLength(str, options) {}
+export function isMobilePhone(str) {
+	return 'No es un número válido';
+}
 
-export function isMobilePhone(str) {}
+export function isMinorTime(minorTime, olderTime) {
+	return `El tiempo ${minorTime} debe ser menor a ${olderTime}`;
+}
 
-export function isMinorTime(minorTime, olderTime) {}
+export function isAlpha(str) {
+	return 'Debe contener solo caracteres del alfabeto';
+}
